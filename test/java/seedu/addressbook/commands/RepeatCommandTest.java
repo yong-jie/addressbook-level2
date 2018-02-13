d6d23aca898736963242df115651ee8f535a5de8
SoraSkyy <viorama@gmail.com>
2018-02-14 01:45:01 +0800

    Add JUnit test for repeat command functionality

---
+++ test/java/seedu/addressbook/commands/RepeatCommandTest.java
@@ -0,0 +1,33 @@
+package seedu.addressbook.commands;
+
+import org.junit.Before;
+import org.junit.Test;
+import seedu.addressbook.parser.Parser;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+public class RepeatCommandTest {
+    private Parser parser;
+
+    @Before
+    public void setUp() {
+        parser = new Parser();
+    }
+
+    @Test
+    public void repeatCommand_invalidCommand_throwsException() {
+        final Command command = this.parser.parseCommand("repeat");
+        final CommandResult result = command.execute();
+        assertEquals(result.feedbackToUser, RepeatCommand.NO_VALID_COMMANDS);
+    }
+
+    @Test
+    public void repeatCommand_repeatCorrectly_throwsException() {
+        this.parser.parseCommand("list");
+        final Command secondCommand = this.parser.parseCommand("repeat");
+        //final CommandResult secondCommandResult = secondCommand.execute();
+        assertThat(secondCommand, instanceOf(ListCommand.class));
+    }
+}
