2d517b889ceca41ff66082efc8b34259e4482d32
SoraSkyy <viorama@gmail.com>
2018-02-14 00:47:41 +0800

    Add help message for repeat command

--- src/seedu/addressbook/commands/HelpCommand.java
+++ src/seedu/addressbook/commands/HelpCommand.java
@@ -24,6 +24,7 @@ public class HelpCommand extends Command {
                 + "\n" + ViewCommand.MESSAGE_USAGE
                 + "\n" + ViewAllCommand.MESSAGE_USAGE
                 + "\n" + HelpCommand.MESSAGE_USAGE
+                + "\n" + RepeatCommand.MESSAGE_USAGE
                 + "\n" + ExitCommand.MESSAGE_USAGE
         );
     }
